9204dbc7720eb8d86f1c044fc743d5657a6b2da8
ziel-main <fazriel.keysha@gmail.com>
2025-02-11 11:30:43 +0700

    js index before after

--- JS dasar/main.js
+++ JS dasar/main.js
@@ -13,3 +13,21 @@
     // console.log('ZeeyFx');
 // } while (i < 10);
 // 
+
+// ARRAY JS PT 2
+const pairAing = [ 'GBPUSD', 'GBPJPY', 'EURJPY', 'AUDUSD', 'USDJPY', 'AUDJPY', 'XAUUSD']
+
+const uj = pairAing.includes('USDJPY');
+
+if (uj) {
+    const posisiUj = pairAing.indexOf('USDJPY');
+    const indexBefore= posisiUj - 1;
+    const indexAfter = posisiUj + 1;
+    const beforeUj = pairAing[indexBefore];
+    const afterUj = pairAing[indexAfter];
+    console.log(`Pair sebelum USDJPY adalah ${beforeUj}`);
+    console.log(`Pair setelah USDJPY adalah ${afterUj}`);
+    console.log(`USDJPY ada dan berada di posisi ${posisiUj}`);
+} else {
+    console.log('USDJPY tidak ada');
+}
