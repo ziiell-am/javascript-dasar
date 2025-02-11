776f6b9d5fd463ee37542f1499f0cf3ac9d9b3cb
ziel-main <fazriel.keysha@gmail.com>
2025-02-11 20:08:09 +0700

    membuat elemen baru di html di digital DOM

--- JS dasar/main.js
+++ JS dasar/main.js
@@ -1,1 +1,19 @@
 // JAVASCRIPT DOM
+
+// MEMBUAT ELEMEN BARU DI HTML (DI DIGITAL DOM)
+const body = document.body;
+
+body.append("Hello World"); //menambahkan teks ke body(append khusus body)
+
+const h1 = document.createElement("h1"); //membuat elemen HTML
+h1.innerHTML = "<marquee>Halo Traders</marquee>"; //menambahkan teks ke elemen HTML
+
+const namaTraders = document.createElement("p"); //membuat elemen HTML
+namaTraders.innerHTML = "<marquee>ZeeyFx</marquee>";
+
+const namaTraders2 = document.createElement("p"); //membuat elemen HTML
+namaTraders2.innerHTML = "<marquee>CryZee</marquee>";
+
+body.append(h1); //menambahkan elemen HTML ke body
+body.append(namaTraders); //menambahkan elemen HTML ke body
+body.append(namaTraders2); //menambahkan elemen HTML ke body
