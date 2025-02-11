ad1217799fc88fd202a5098579c3745fd0446e70
ziel-main <fazriel.keysha@gmail.com>
2025-02-11 11:42:18 +0700

    array alicia key

--- JS dasar/main.js
+++ JS dasar/main.js
@@ -15,22 +15,6 @@
 // 
 
 // ARRAY JS PT 2
-const pairAing = [ 'GBPUSD', 'GBPJPY', 'EURJPY', 'AUDUSD', 'USDJPY', 'AUDJPY', 'XAUUSD']
+const pairAing = [] //array alicia key
 
-const uj = pairAing.includes('USDJPY');
-
-if (uj) {
-    const posisiUj = pairAing.indexOf('USDJPY');
-    const indexBefore= posisiUj - 1;
-    const indexAfter = posisiUj + 1;
-    const beforeUj = pairAing[indexBefore];
-    const afterUj = pairAing[indexAfter];
-    console.log(`Pair sebelum USDJPY adalah ${beforeUj}`);
-    console.log(`Pair setelah USDJPY adalah ${afterUj}`);
-    const pairAwal = pairAing.shift();
-    console.log(`Pair awal adalah ${pairAwal}`);
-    const pairAkhir = pairAing.pop();
-    console.log(`Pair akhir adalah ${pairAkhir}`);
-} else {
-    console.log('USDJPY tidak ada');
-}
+pairAing['UJ'] = "USDJPY"
